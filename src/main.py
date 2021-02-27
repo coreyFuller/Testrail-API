127aadc4e8cc1300d8f69a54a827134c72043df1
Corey Fuller <coreyfuller00@gmail.com>
2021-02-27 14:35:25 -0500

    adding main function

--- src/main.py
+++ src/main.py
@@ -1,11 +1,15 @@
 from testrail import *
 from functions import *
 
-projects = getProjects()
-for project in projects:
-    id = project["id"]
-    print(getProject(id))
 
-pass
+def main():
+    projects = getProjects()
+    for project in projects:
+        id = project["id"]
+        print(getProject(id))
+    pass
 
+
+if __name__ == '__main__':
+    main()
 
